c4182987ef82a3c5cb3950d857dec3f8324badbc
李志钟 <aichaellee@lizhizhongdeMacBook-Pro.local>
2018-11-19 15:26:02 +0800

    sleep

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -52,7 +52,10 @@ public class Main {
   }
 
   @GetMapping(value = "/vpn")
-  String concurrency(String threadId) {
+  String concurrency(String threadId) throws Exception {
+    if(threadId.equals("10") || threadId.equals("20")){
+      Thread.sleep(10000);
+    }
     return "the threadId is : "+threadId+"  and time: "+ new Timestamp(System.currentTimeMillis());
   }
 
